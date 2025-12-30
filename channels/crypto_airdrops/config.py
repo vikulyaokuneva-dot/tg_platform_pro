6d5c109dbc727dedd111ac0c1996f20fd44742df
vikulyaokuneva-dot <vikulyaokuneva@gmail.com>
2025-12-30 21:40:41 +0300

    Add files via upload

---
+++ channels/crypto_airdrops/config.py
@@ -0,0 +1,12 @@
+
+BOT_TOKEN = "PUT_YOUR_TOKEN"
+CHAT_ID = "@your_channel"
+ENABLED = True
+
+RSS_SOURCES = [
+    "https://cryptoslate.com/feed/"
+]
+
+INTERVAL_MINUTES = 30
+MAX_POSTS_PER_CYCLE = 3
+POST_DELAY = 5
