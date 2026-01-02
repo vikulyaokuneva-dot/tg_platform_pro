07f576cc67e2c8d0314741c1555423589b6c7bbb
vikulyaokuneva-dot <vikulyaokuneva@gmail.com>
2026-01-03 01:00:06 +0300

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -9,4 +9,9 @@ CHANNELS = {
     "ai_automation": os.getenv("AI_AUTOMATION_CHAT_ID"),
     "personal_finance": os.getenv("PERSONAL_FINANCE_CHAT_ID"),
     "stocks_investing": os.getenv("STOCKS_INVESTING_CHAT_ID"),
+    "startups_vc": os.getenv("STARTUPS_VC_CHAT_ID"),
+    "product_growth": os.getenv("PRODUCT_GROWTH_CHAT_ID"),
+    "programming_dev": os.getenv("PROGRAMMING_DEV_CHAT_ID"),
+    "anekdoty": os.getenv("ANEKDOTY_CHAT_ID"),
 }
+
