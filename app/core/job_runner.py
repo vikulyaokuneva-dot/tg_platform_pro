d83e029f4f5c36ae21a4bfcdb4cb9269d8eb7ae2
vikulyaokuneva-dot <vikulyaokuneva@gmail.com>
2026-01-12 16:31:39 +0300

    Update job_runner.py

--- app/core/job_runner.py
+++ app/core/job_runner.py
@@ -1,14 +1,24 @@
+from app.parsers.html_parser import load_html
+from app.extractors.article import extract_article
+from app.jobs.templates import build_article_post
 from app.core.publisher import publish_job
-from app.jobs.templates import build_simple_post
+
+
+URLS = [
+    "https://example.com/article1",
+    "https://example.com/article2",
+]
 
 
 def run_all_jobs():
-    job = {
-        "name": "content",
-        "builder": build_simple_post,
-        "content": {
-            "title": "Платформа запущена",
-            "text": "Telegram Content Platform работает корректно 🚀"
+    for url in URLS:
+        soup = load_html(url)
+        content = extract_article(soup)
+
+        job = {
+            "name": "article",
+            "builder": build_article_post,
+            "content": content,
         }
-    }
-    publish_job('content', job)
+
+        publish_job("article", job)
