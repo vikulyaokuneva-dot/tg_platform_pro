f0630ada7529089d1ef32c568935d43425abf21e
vikulyaokuneva-dot <vikulyaokuneva@gmail.com>
2026-01-12 16:15:55 +0300

    Update publisher.py

--- app/core/publisher.py
+++ app/core/publisher.py
@@ -2,9 +2,5 @@ from app.core.telegram import send_post
 
 
 def publish_job(name, job):
-    channel = job.get('channel')
-    if not channel:
-        print('[SKIP] Channel None')
-        return
-    text = job['builder'](job)
-    send_post(channel, text)
+    text = job["builder"](job)
+    send_post(text)
