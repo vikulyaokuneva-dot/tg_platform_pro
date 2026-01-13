2ddc0ea1f107bf2080e834ea3fad26932e6843ec
vikulyaokuneva-dot <vikulyaokuneva@gmail.com>
2026-01-13 17:46:16 +0300

    Update html_parser.py

--- app/parsers/html_parser.py
+++ app/parsers/html_parser.py
@@ -9,3 +9,4 @@ def load(url: str) -> BeautifulSoup:
     response = requests.get(url, headers=headers, timeout=20)
     response.raise_for_status()
     return BeautifulSoup(response.text, "lxml")
+
